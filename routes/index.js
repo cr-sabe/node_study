0ded642346a2a1fedf5d98d1afabe4dba647075f
cr-sabe <s_abe@cosmoroot.co.jp>
2021-03-23 18:03:51 +0900

    全国選択

--- routes/index.js
+++ routes/index.js
@@ -2,27 +2,39 @@ var express = require('express');
 var request = require('request');
 var router = express.Router();
 
-const APP_ID = "9d09c0e2d2d4ea3c04438c7ff433bfa8";
+const APP_ID = "APIKEY";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	var option = {
-		url: "https://api.openweathermap.org/data/2.5/onecall",
-		method: "GET",
-		qs: {
-			lat: 35.18028,
-			lon: 136.90667,
-			appid: APP_ID,
-			lang: "ja",
-			units: "metric"
-		}
-	};
-	request(option, function (error, response, body) {
-		var json = JSON.parse(body);
-		res.render('index', {
-			current: json.current.weather[0].description
+	var mapInfos = require('../public/map.json');
+	if (req.query.location) {
+		var mapInfo = mapInfos.find(x => x.name == req.query.location);
+		var option = {
+			url: "https://api.openweathermap.org/data/2.5/onecall",
+			method: "GET",
+			qs: {
+				lat: mapInfo.lat,
+				lon: mapInfo.lon,
+				appid: APP_ID,
+				lang: "ja",
+				units: "metric"
+			}
+		};
+		request(option, function (error, response, body) {
+			var json = JSON.parse(body);
+			res.render('index', {
+				mapInfos: null,
+				name: mapInfo.name,
+				current: json.current.weather[0]
+			});
 		});
-	});
+	} else {
+		res.render('index', {
+			mapInfos: mapInfos,
+			name: null,
+			current: null
+		});
+	}
 });
 
 module.exports = router;
