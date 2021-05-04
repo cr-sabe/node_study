6568eee20814a4afb1c407d68f35e08837beb54a
cr-sabe <s_abe@cosmoroot.co.jp>
2021-05-04 20:41:05 +0900

    内部でAPI呼び出し集約

--- routes/index.js
+++ routes/index.js
@@ -1,40 +1,42 @@
-var express = require('express');
-var request = require('request');
+var express = require("express");
+var fetch = require("node-fetch");
+var setting = require("../public/setting.json");
 var router = express.Router();
 
-const APP_ID = "APIKEY";
-
 /* GET home page. */
-router.get('/', function(req, res, next) {
-	var mapInfos = require('../public/map.json');
-	if (req.query.location) {
-		var mapInfo = mapInfos.find(x => x.name == req.query.location);
-		var option = {
-			url: "https://api.openweathermap.org/data/2.5/onecall",
-			method: "GET",
-			qs: {
-				lat: mapInfo.lat,
-				lon: mapInfo.lon,
-				appid: APP_ID,
-				lang: "ja",
-				units: "metric"
-			}
-		};
-		request(option, function (error, response, body) {
-			var json = JSON.parse(body);
-			res.render('index', {
-				mapInfos: null,
-				name: mapInfo.name,
-				current: json.current.weather[0]
-			});
-		});
-	} else {
-		res.render('index', {
-			mapInfos: mapInfos,
-			name: null,
-			current: null
-		});
-	}
+router.get("/", function (req, res, next) {
+  var promiseList = [];
+  setting.mapInfos.forEach((mapInfo) => {
+    var params = new URLSearchParams();
+    params.append("appid", setting.apikey);
+    params.append("id", mapInfo.cityID);
+    params.append("lang", "ja");
+    params.append("units", "metric");
+
+    promiseList.push(
+      fetch("https://api.openweathermap.org/data/2.5/weather?" + params, {
+        method: "GET",
+      }).then(res => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error(`${res.status} ${res.statusText}`);
+        }
+      }).then(json => {
+		return {
+            name: mapInfo.name, // 都道府県名
+            weather: json.weather.description, // 天気名称
+            temp: json.main.temp, // 現在気温
+            tempMin: json.main.temp_min, // 最低気温
+            tempMax: json.main.temp_max, // 最高気温
+          };
+	  })
+    );
+  });
+
+  Promise.all(promiseList).then(dataList => {
+    res.render("index", { dataList: dataList });
+  });
 });
 
 module.exports = router;
