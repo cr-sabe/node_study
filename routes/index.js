fee7abf4975a7362942de82ba3c33612ded17fc2
s.abe <tanaka.yamada@gmail.com>
2021-05-30 15:41:03 +0900

    各都市の表示

--- routes/index.js
+++ routes/index.js
@@ -7,31 +7,34 @@ var router = express.Router();
 router.get("/", function (req, res, next) {
   var promiseList = [];
   setting.mapInfos.forEach((mapInfo) => {
-    var params = new URLSearchParams();
-    params.append("appid", setting.apikey);
-    params.append("id", mapInfo.cityID);
-    params.append("lang", "ja");
-    params.append("units", "metric");
+    if (mapInfo.mapping) {
+      var params = new URLSearchParams();
+      params.append("appid", setting.apikey);
+      params.append("id", mapInfo.cityID);
+      params.append("lang", "ja");
+      params.append("units", "metric");
 
-    promiseList.push(
-      fetch("https://api.openweathermap.org/data/2.5/weather?" + params, {
-        method: "GET",
-      }).then(res => {
-        if (res.ok) {
-          return res.json();
-        } else {
-          throw new Error(`${res.status} ${res.statusText}`);
-        }
-      }).then(json => {
-		return {
+      promiseList.push(
+        fetch("https://api.openweathermap.org/data/2.5/weather?" + params, {
+          method: "GET",
+        }).then(res => {
+          if (res.ok) {
+            return res.json();
+          } else {
+            throw new Error(`${res.status} ${res.statusText}`);
+          }
+        }).then(json => {
+          return {
             name: mapInfo.name, // 都道府県名
+            x: mapInfo.x, // X座標
+            y: mapInfo.y, // Y座標
             weather: json.weather.description, // 天気名称
-            temp: json.main.temp, // 現在気温
-            tempMin: json.main.temp_min, // 最低気温
-            tempMax: json.main.temp_max, // 最高気温
+            tempMin: Number(json.main.temp_min).toFixed(1), // 最低気温
+            tempMax: Number(json.main.temp_max).toFixed(1), // 最高気温
           };
-	  })
-    );
+        })
+      );
+    }
   });
 
   Promise.all(promiseList).then(dataList => {
